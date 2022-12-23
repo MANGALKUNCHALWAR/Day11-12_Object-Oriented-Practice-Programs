484f68f40fbc061c81e1b76a4b5ebbc8175366ac
John Doe <mangalkunchalwar9999@gmail.com>
2022-12-23 11:03:18 +0530

    Welcome to the Object-Oriented-Practice-Programs

---
+++ Main.java
@@ -0,0 +1,7 @@
+package assignment11and12.ObjectOrientedPracticePrograms;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Object-Oriented-Practice-Programs");
+    }
+}
